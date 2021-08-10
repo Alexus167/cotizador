a1c5f8d6bf5a222b8792c24516c110c424e11117
Alexus167 <alexivanmendez08@gmail.com>
2021-08-10 02:04:04 -0300

    Cotizador listo

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Spinner.css'
+
+const Spinner = () => {
+    return ( 
+       <div className="spinner">
+        <div className="cube1" />
+        <div className="cube2" />
+       </div>
+
+     );
+}
+ 
+export default Spinner;
